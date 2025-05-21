494967a00fdddb60c87d229f1c751345357b20cf
0w0n2 <dahxtq1@naver.com>
2025-05-22 06:47:03 +0900

    wip: security 구현 중, rest controller helper 추가 중

--- src/main/java/com/ssafy/home/user/controller/HouseController.java
+++ src/main/java/com/ssafy/home/user/controller/HouseController.java
@@ -15,11 +15,14 @@ import com.ssafy.home.user.model.dto.HouseDealResponseDto;
 import com.ssafy.home.user.model.dto.SearchHouseResponseDto;
 import com.ssafy.home.user.model.service.HouseService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController // @RequestBody 생략 가능
-@RequestMapping("/houses")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/houses")
+@Tag(name = "HouseController", description = "실거래가 관련 기능 처리")
 public class HouseController {
 	
 	private final HouseService houseService;
@@ -29,25 +32,13 @@ public class HouseController {
 		return ResponseEntity.ok(ApiResponse.success());
 	}
 	
-	/**
-	 * [URL]
-	 * 실거래가 정보 - /deals 
-	 */
 	
 	// 검색어(아파트명) 으로 일치하는 아파트 리스트 조회 
 	// GET /houses/search?keyword=한신
 	@GetMapping("/search")
+	@Operation(summary = "검색어로 아파트 조회", description = "아파트명으로 검색 시, 해당 키워드를 포함한 apt_seq을 가진 매물 정보 리스트를 반환한다.")
 	public ResponseEntity<?> searchHousesByKeyword(@RequestParam String keyword){ 
-//			ApartmentSearchRequestDto requestDto = ApartmentSearchRequestDto.builder()
-//					.keyword(keyword)
-//					.build();
-		
-		List<SearchHouseResponseDto> result = houseService.searchHousesByKeyword(keyword);  
-		
-		// 조회 정보 없음
-//			if (result==null) return ResponseEntity.status(HttpStatus.NOT_FOUND) // 404
-//					.body("조회된 데이터가 없습니다.");
-//					// .build(); // 404
+		List<SearchHouseResponseDto> result = houseService.searchHousesByKeyword(keyword);  	
 		
 		return ResponseEntity.status(HttpStatus.OK).body(result);
 	}
@@ -62,5 +53,14 @@ public class HouseController {
 	
 	// /api/houses/{houseId}/deals?sort=price_desc
 	// /api/houses/{houseId}/deals?year=2024&month=3
+	
+//	ApartmentSearchRequestDto requestDto = ApartmentSearchRequestDto.builder()
+//	.keyword(keyword)
+//	.build();
+	
+	// 조회 정보 없음
+//	if (result==null) return ResponseEntity.status(HttpStatus.NOT_FOUND) // 404
+//			.body("조회된 데이터가 없습니다.");
+//			// .build(); // 404
 	
 }
